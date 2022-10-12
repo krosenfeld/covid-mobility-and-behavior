7c9473ffa0eedd1db72549e3cf78da283e840eb0
Katherine Rosenfeld <krosenfeld@idmod.org>
2022-10-12 11:34:47 -0700

    add mirror info

--- install.R
+++ install.R
@@ -1,3 +1,7 @@
+local({r <- getOption("repos")
+       r["CRAN"] <- "http://cran.r-project.org" 
+       options(repos=r)
+})
 install.packages("mclust", requirements=TRUE)
 install.packages("dplyr", requirements=TRUE)
 install.packages("tidycensus", requirements=TRUE)
